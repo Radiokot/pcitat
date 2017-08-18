3550a87398fed5f2b533e03e6c6d44ab5ab50e4d
Oleg Koretsky <test.radiokot@gmail.com>
2017-08-18 12:26:14 +0300

    Add login endpoint

---
+++ api/ApiHelper.php
@@ -0,0 +1,50 @@
+<?php
+
+define("ERROR_BAD_REQUEST",     400);
+define("ERROR_NOT_AUTHORIZED",  401);
+define("ERROR_NOT_FOUND",       404);
+define("ERROR_CONFLICT",        409);
+
+define("PC_USER", "pc_user");
+
+// CORS.
+header("Access-Control-Allow-Origin: *");
+if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
+
+    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"])) {
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    }         
+
+    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"])) {
+        header("Access-Control-Allow-Headers: {$_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"]}");
+    }
+
+    exit();
+}
+
+function getJsonBody() {
+    return json_decode(file_get_contents("php://input"), true);
+}
+
+function response($data) {
+    header("Content-Type: application/json");
+    echo(json_encode([
+        "response" => $data
+    ]));
+    exit();
+}
+
+function responseArray($array) {
+    response([
+        "count" => count($array),
+        "items" => $array
+    ]);
+}
+
+function error($code) {
+    http_response_code($code);
+    header("Content-Type: application/json");
+    exit();
+}
+
+?>
---
+++ api/login.php
@@ -0,0 +1,37 @@
+<?php
+
+include_once "../php/UserManager.php";
+include_once dirname(__FILE__)."/ApiHelper.php";
+error_reporting(E_ALL & ~E_DEPRECATED);
+
+session_start();
+
+$bodyJson = getJsonBody();
+if ($_SERVER["REQUEST_METHOD"] !== "POST" || $bodyJson === null) {
+    error(ERROR_BAD_REQUEST);
+    exit();
+}
+
+$email = mysql_escape_string((isset($bodyJson["email"])) ? trim($bodyJson["email"]) : "");
+$password = mysql_escape_string((isset($bodyJson["password"])) ? trim($bodyJson["password"]) : "");
+
+$user = UserManager::login(["email" => $email, "password" => $password]);
+
+if ($user === null) {
+    error(ERROR_NOT_FOUND);
+}
+
+$_SESSION[PC_USER] = $user;
+
+response([
+    "id" => $user["id"],
+    "email" => $user["email"],
+    "name" => $user["name"],
+    "twitter" => [
+        "username" => $user["twitter_name"],
+        "book" => $user["twitter_book_id"]
+    ],
+    "session" => session_id()
+]);
+
+?>
