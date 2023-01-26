898c1253168d7ffe33a005248268b1fd178fad10
Oleg Koretsky <oleg@radiokot.com.ua>
2023-01-26 21:16:41 +0200

    Add get book by ID public endpoint

--- api/publicBooks.php
+++ api/publicBooks.php
@@ -5,14 +5,23 @@ include_once dirname(__FILE__)."/ApiHelper.php";
 error_reporting(E_ALL & ~E_DEPRECATED);
 
 if ($_SERVER["REQUEST_METHOD"] === "GET") {
+    $bookId = ((isset($_REQUEST["id"])) ? trim($_REQUEST["id"]) : "");
     $userId = ((isset($_REQUEST["user"])) ? trim($_REQUEST["user"]) : "");
 
-    if ($userId === "") {
+    if ($userId === "" && $bookId === "") {
         error(ERROR_BAD_REQUEST);
     }
 
-    $books = BookManager::getPublicByUserId($userId);
-    responseArray($books);
+    if ($bookId === "") {
+        $books = BookManager::getPublicByUserId($userId);
+        responseArray($books);
+    } else {
+        $book = BookManager::getById($bookId);
+        if ($book == null) {
+            error(ERROR_NOT_FOUND);
+        }        
+        response($book);
+    }
 } else {
     error(ERROR_BAD_REQUEST);
 }
